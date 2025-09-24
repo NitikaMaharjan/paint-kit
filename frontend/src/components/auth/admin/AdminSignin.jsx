93a515089055cb3e78f9769381cce528380dde82
Keppiekip <maharjanitika@gmail.com>
2025-09-24 12:42:53 +0545

    refactor: admin signout

--- frontend/src/components/auth/admin/AdminSignin.jsx
+++ frontend/src/components/auth/admin/AdminSignin.jsx
@@ -34,8 +34,8 @@ export default function AdminSignin() {
       const json = await response.json();
 
       if(json.success){
-        alert("Welcome back!" + json.authtoken);
-        localStorage.setItem("signedIn", "true");
+        alert("You've signed in. Welcome back!" + json.authtoken);
+        localStorage.setItem("adminSignedIn", "true");
       }else{
         alert(json.error);
       }
@@ -44,37 +44,53 @@ export default function AdminSignin() {
       alert("Network error. Please check your connection or try again later!")
     }
   }
+  
+  const handleSignOut = ()=> {
+    let ans = window.confirm("Are you sure?");
+    if (ans) {
+      localStorage.removeItem("adminSignedIn");
+      alert("You've signed out. See you next time!");
+    }
+  }
 
   return (
-    <div className="auth-form-box">
-      <div className="flex justify-center gap-1">
-        <img src="logo.png" style={{height: "28px", width: "28px"}}/>
-        <h1 style={{fontSize: "14px", padding: "6px 0px 0px 0px"}}><b>Paint Kit</b></h1>
+    <>
+      {
+        localStorage.getItem("adminSignedIn")?
+          <button style={{position: "fixed", top: "32px", right: "32px"}} onClick={handleSignOut}><b>Sign out</b></button>
+          :
+          <></>
+      }
+      <div className="auth-form-box">
+        <div className="flex justify-center gap-1">
+          <img src="logo.png" style={{height: "28px", width: "28px"}}/>
+          <h1 style={{fontSize: "14px", padding: "6px 0px 0px 0px"}}><b>Paint Kit</b></h1>
+        </div>
+        <h1 style={{margin: "12px 0px 14px 0px", fontSize: "15px", textAlign: "center"}}><b>Welcome back</b></h1>
+        <form className="auth-form">
+          <div className="mb-1">
+            <label htmlFor="email"><b>Email</b></label>
+            <div className="input-bar">
+              <input type="text" id="email" name="email" placeholder="Enter email" value={credentials.email} onChange={updateInputValue} autoComplete="on"/>
+              <img src="close.png" onClick={() => {clearInput("email")}} style={{opacity: `${credentials.email===""?0:1}`}}/>
+            </div>
+          </div>          
+          <div style={{marginBottom: "28px"}}>
+            <div className="flex items-center justify-between pr-1">
+              <label htmlFor="password"><b>Password</b></label>
+              <img src={`${passwordType==="password"?"hide":"show"}.png`} style={{height: "16px", width: "16px"}} onClick={()=>{changePasswordType()}}/>
+            </div>
+            <div className="input-bar">
+              <input type={`${passwordType}`} id="password" name="password" placeholder="Enter password" value={credentials.password} onChange={updateInputValue}/>
+              <img src="close.png" onClick={() => {clearInput("password")}} style={{opacity: `${credentials.password===""?0:1}`}}/>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center">
+            <button type="submit" className="submit-btn" onClick={handleSubmit}><b>Sign in</b></button>
+            <p style={{marginTop: "6px",textAlign: "center",fontSize: "13px"}}>Don't have an account? <span style={{borderBottom: "1px solid black"}}>Sign up</span></p>
+          </div>
+        </form>
       </div>
-      <h1 style={{margin: "12px 0px 14px 0px", fontSize: "15px", textAlign: "center"}}><b>Welcome back</b></h1>
-      <form className="auth-form">
-        <div className="mb-1">
-          <label htmlFor="email"><b>Email</b></label>
-          <div className="input-bar">
-            <input type="text" id="email" name="email" placeholder="Enter email" value={credentials.email} onChange={updateInputValue} autoComplete="on"/>
-            <img src="close.png" onClick={() => {clearInput("email")}} style={{opacity: `${credentials.email===""?0:1}`}}/>
-          </div>
-        </div>          
-        <div style={{marginBottom: "28px"}}>
-          <div className="flex items-center justify-between pr-1">
-            <label htmlFor="password"><b>Password</b></label>
-            <img src={`${passwordType==="password"?"hide":"show"}.png`} style={{height: "16px", width: "16px"}} onClick={()=>{changePasswordType()}}/>
-          </div>
-          <div className="input-bar">
-            <input type={`${passwordType}`} id="password" name="password" placeholder="Enter password" value={credentials.password} onChange={updateInputValue}/>
-            <img src="close.png" onClick={() => {clearInput("password")}} style={{opacity: `${credentials.password===""?0:1}`}}/>
-          </div>
-        </div>
-        <div className="flex flex-col justify-center">
-          <button type="submit" className="submit-btn" onClick={handleSubmit}><b>Sign in</b></button>
-          <p style={{marginTop: "6px",textAlign: "center",fontSize: "13px"}}>Don't have an account? <span style={{borderBottom: "1px solid black"}}>Sign up</span></p>
-        </div>
-      </form>
-    </div>
+    </>
   )
 }
