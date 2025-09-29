2afe5d9626f9af1ac58088d9a2975abbb2401456
Keppiekip <maharjanitika@gmail.com>
2025-09-29 12:30:15 +0545

    style: alert ui

--- frontend/src/context/alert/AlertState.jsx
+++ frontend/src/context/alert/AlertState.jsx
@@ -30,16 +30,20 @@ export default function AlertState(props) {
                 
                 &&
 
-                <div className="alert-modal-background">
+                <div className="alert-modal-background" onClick={()=>{setAlert(false)}}>
                     <div className="alert-modal">
-                        <div className="flex items-center justify-between" style={{padding: "6px 12px", borderBottom: "1px solid black", backgroundColor: `${alertColor}`}}>
-                            <div className="flex items-center justify-between gap-2">
+                        <div className="flex items-center justify-between" style={{borderBottom: "1px solid black", backgroundColor: `${alertColor}`}}>
+                            <div className="flex items-center justify-between gap-2" style={{padding: "0px 0px 0px 8px"}}>
                                 <img src={alertType+".png"} alt={alertType+" image"} style={{width: "18px"}}/>
-                                <h1 style={{paddingTop: "3px", fontSize: "14px", color: "black"}}><b>{alertType}</b></h1>
+                                <h1 style={{paddingTop: "2px", fontSize: "13px", color: "black"}}><b>{alertType}</b></h1>
                             </div>
-                            <img src="close.png" alt="close button image" style={{height: "14px", width: "14px", cursor: "pointer"}} onClick={()=>{setAlert(false)}}/>
+                            <div style={{borderLeft: "1px solid black"}}>
+                                <div style={{padding: "6px"}}>
+                                    <img src="close.png" alt="close button image" style={{height: "13px", width: "13px", cursor: "pointer"}} onClick={()=>{setAlert(false)}}/>
+                                </div>
+                            </div>
                         </div>
-                        <p style={{padding: "12px", fontSize: "13px"}}>{alertMsg}</p>
+                        <p style={{padding: "8px", fontSize: "13px"}}>{alertMsg}</p>
                     </div>
                 </div>
             }
