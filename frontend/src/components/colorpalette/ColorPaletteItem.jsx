2879881218cf49c453f74cec7499c8849c8ebe7f
Keppiekip <maharjanitika@gmail.com>
2025-10-05 21:11:51 +0545

    style: user color palette

--- frontend/src/components/colorpalette/ColorPaletteItem.jsx
+++ frontend/src/components/colorpalette/ColorPaletteItem.jsx
@@ -1,9 +1,15 @@
 export default function ColorPaletteItem(props) {
     const { color_palette_name, colors } = props;
     return (
-        <div>
-            <p>{color_palette_name}</p>
-            {colors.map((a_color, index)=>{return <div key={index} style={{height: "24px", width: "24px", backgroundColor: `${a_color}`}}></div>})}
+        <div className="color-palette-item">
+            <div style={{padding: "12px 12px 4px 12px"}}>
+                <p title={color_palette_name}style={{fontSize: "13px"}}>{color_palette_name.slice(0,14)+"..."}</p>
+            </div>
+            <div style={{padding: "0px 12px 12px 12px"}}>
+                <div style={{display: "grid", gridTemplateColumns: "repeat(6, 1fr)", gap: "2px"}}>
+                    {colors.map((a_color, index)=>{return <div key={index} title={a_color} style={{height: "32px", width: "32px", backgroundColor: `${a_color}`}}></div>}).reverse()}
+                </div>
+            </div>
         </div>
     )
 }
--- frontend/src/components/user/UserViewColorPalette.jsx
+++ frontend/src/components/user/UserViewColorPalette.jsx
@@ -1,10 +1,12 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import ColorPaletteDetailsContext from "../../context/colorpalette/ColorPaletteDetailsContext";
 import ColorPaletteItem from "../colorpalette/ColorPaletteItem";
 
 export default function UserViewColorPalette() {
 
   const { adminColorPaletteDetails, adminFetchColorPalette, userColorPaletteDetails, userFetchUserColorPalette } = useContext(ColorPaletteDetailsContext);
+
+  const [showColorPalette, setShowColorPalette] = useState("community palettes");
   
   useEffect(() => {
     if (localStorage.getItem("userSignedIn")){
@@ -14,27 +16,33 @@ export default function UserViewColorPalette() {
   }, []);
   
   return (
-    <>
+    <div className="color-palette">
+      <button onClick={()=>{setShowColorPalette("community palettes")}}>Community</button>
+      <button onClick={()=>{setShowColorPalette("my palettes")}}>Mine</button>
       {
-        adminColorPaletteDetails.length === 0 ?
-          <></>
+        showColorPalette === "community palettes"?
+          adminColorPaletteDetails.length === 0 ?
+            <div>
+              <p>nothing to show</p>
+            </div>
+          :
+            <div>
+              {(adminColorPaletteDetails).map((colorpalette)=>{
+                    return <ColorPaletteItem key={colorpalette._id} color_palette_name={colorpalette.color_palette_name} colors={colorpalette.colors}/>
+                })}
+            </div>
         :
-          <div>
-            {(adminColorPaletteDetails).map((colorpalette)=>{
-                  return <ColorPaletteItem key={colorpalette._id} color_palette_name={colorpalette.color_palette_name} colors={colorpalette.colors}/>
-              })}
-          </div>
+          userColorPaletteDetails.length === 0 ?
+            <div>
+              <p>get started with your own custom palettes</p>
+            </div>
+          :
+            <div>
+              {(userColorPaletteDetails).map((colorpalette)=>{
+                    return <ColorPaletteItem key={colorpalette._id} color_palette_name={colorpalette.color_palette_name} colors={colorpalette.colors}/>
+                })}
+            </div>
       }
-      {
-        userColorPaletteDetails.length === 0 ?
-          <></>
-        :
-          <div>
-            {(userColorPaletteDetails).map((colorpalette)=>{
-                  return <ColorPaletteItem key={colorpalette._id} color_palette_name={colorpalette.color_palette_name} colors={colorpalette.colors}/>
-              })}
-          </div>
-      }
-    </>
+    </div>
   )
 }
