f16a02b7469cc84f17f40f231c0497ad75234c37
Keppiekip <maharjanitika@gmail.com>
2026-01-20 07:51:00 +0545

    refactor: add title for tools

--- frontend/src/components/navbar/LeftNavbar.jsx
+++ frontend/src/components/navbar/LeftNavbar.jsx
@@ -9,67 +9,67 @@ export default function LeftNavbar(props) {
     <div className="left-navbar">
       <div className="left-tool-bar">
         <div className="flex" style={{borderBottom: "1px solid black"}}>
-          <div className={`tool ${tool==="pen"?"selected-tool":""}`} style={{borderRight: "1px solid black"}} onClick={()=>{setTool("pen")}}>
+          <div className={`tool ${tool==="pen"?"selected-tool":""}`} style={{borderRight: "1px solid black"}} onClick={()=>{setTool("pen")}} title="pen tool">
             <img src="/pen.png" alt="pen icon" style={{height: "20px", width: "20px"}}/>
           </div>
-          <div className={`tool ${tool==="eraser"?"selected-tool":""}`} onClick={()=>{setTool("eraser")}}>
+          <div className={`tool ${tool==="eraser"?"selected-tool":""}`} onClick={()=>{setTool("eraser")}} title="eraser tool">
             <img src="/eraser.png" alt="eraser icon" style={{height: "20px", width: "20px"}}/>
           </div>
         </div>
         <div className="flex" style={{borderBottom: "1px solid black"}}>
-          <div className={`tool ${tool==="bucket"?"selected-tool":""}`} style={{borderRight: "1px solid black"}} onClick={()=>{if(props.checkUserSignedIn()){setTool("bucket")}}}>
+          <div className={`tool ${tool==="bucket"?"selected-tool":""}`} style={{borderRight: "1px solid black"}} onClick={()=>{if(props.checkUserSignedIn()){setTool("bucket")}}} title="bucket tool">
             <img src="/bucket.png" alt="bucket icon" style={{height: "20px", width: "20px"}}/>
           </div>
-          <div className={`tool ${tool==="bucketeraser"?"selected-tool":""}`} onClick={()=>{if(props.checkUserSignedIn()){setTool("bucketeraser")}}}>
+          <div className={`tool ${tool==="bucketeraser"?"selected-tool":""}`} onClick={()=>{if(props.checkUserSignedIn()){setTool("bucketeraser")}}} title="bucket eraser tool">
             <img src="/eraser.png" alt="eraser icon" style={{height: "18px", width: "18px"}}/>
             <img src="/bucket.png" alt="bucket icon" style={{height: "18px", width: "18px"}}/>
           </div>
         </div>
         <div className="flex" style={{borderBottom: "1px solid black"}}>
-          <div className={`tool ${tool==="text"?"selected-tool":""}`} style={{borderRight: "1px solid black"}} onClick={()=>{if(props.checkUserSignedIn()){setTool("text")}}}>
+          <div className={`tool ${tool==="text"?"selected-tool":""}`} style={{borderRight: "1px solid black"}} onClick={()=>{if(props.checkUserSignedIn()){setTool("text")}}} title="text tool">
             <img src="/text.png" alt="text icon" style={{height: "25px", width: "25px"}}/>
           </div>
-          <div className={`tool ${tool==="line"?"selected-tool":""}`} onClick={()=>{if(props.checkUserSignedIn()){setTool("line")}}}>
+          <div className={`tool ${tool==="line"?"selected-tool":""}`} onClick={()=>{if(props.checkUserSignedIn()){setTool("line")}}} title="line tool">
             <img src="/line.png" alt="line icon" style={{height: "20px", width: "20px"}}/>
           </div>
         </div>
         <div className="flex" style={{borderBottom: "1px solid black"}}>
-          <div className={`tool ${tool==="circle"?"selected-tool":""}`} style={{borderRight: "1px solid black"}} onClick={()=>{if(props.checkUserSignedIn()){setTool("circle")}}}>
+          <div className={`tool ${tool==="circle"?"selected-tool":""}`} style={{borderRight: "1px solid black"}} onClick={()=>{if(props.checkUserSignedIn()){setTool("circle")}}} title="circle tool">
             <img src="/circle.png" alt="circle icon" style={{height: "20px", width: "20px"}}/>
           </div>
-          <div className={`tool ${tool==="square"?"selected-tool":""}`} onClick={()=>{if(props.checkUserSignedIn()){setTool("square")}}}>
+          <div className={`tool ${tool==="square"?"selected-tool":""}`} onClick={()=>{if(props.checkUserSignedIn()){setTool("square")}}} title="square tool">
             <img src="/square.png" alt="square icon" style={{height: "20px", width: "20px"}}/>
           </div>
         </div>
         <div className="flex" style={{borderBottom: "1px solid black"}}>
-          <div className={`tool ${tool==="rectangle"?"selected-tool":""}`} style={{borderRight: "1px solid black"}} onClick={()=>{if(props.checkUserSignedIn()){setTool("rectangle")}}}>
+          <div className={`tool ${tool==="rectangle"?"selected-tool":""}`} style={{borderRight: "1px solid black"}} onClick={()=>{if(props.checkUserSignedIn()){setTool("rectangle")}}} title="rectangle tool">
             <img src="/rectangle.png" alt="rectangle icon" style={{height: "22px", width: "22px"}}/>
           </div>
-          <div className={`tool ${tool==="triangle"?"selected-tool":""}`} onClick={()=>{if(props.checkUserSignedIn()){setTool("triangle")}}}>
+          <div className={`tool ${tool==="triangle"?"selected-tool":""}`} onClick={()=>{if(props.checkUserSignedIn()){setTool("triangle")}}} title="triangle tool">
             <img src="/triangle.png" alt="triangle icon" style={{height: "23px", width: "23px"}}/>
           </div>
         </div>
         <div className="flex" style={{borderBottom: "1px solid black"}}>
-          <div className={`tool ${tool==="upparabola"?"selected-tool":""}`} style={{borderRight: "1px solid black"}} onClick={()=>{if(props.checkUserSignedIn()){setTool("upparabola")}}}>
+          <div className={`tool ${tool==="upparabola"?"selected-tool":""}`} style={{borderRight: "1px solid black"}} onClick={()=>{if(props.checkUserSignedIn()){setTool("upparabola")}}} title="up parabola tool">
             <img src="/upparabola.png" alt="up parabola icon" style={{height: "16px", width: "16px"}}/>
           </div>
-          <div className={`tool ${tool==="downparabola"?"selected-tool":""}`} onClick={()=>{if(props.checkUserSignedIn()){setTool("downparabola")}}}>
+          <div className={`tool ${tool==="downparabola"?"selected-tool":""}`} onClick={()=>{if(props.checkUserSignedIn()){setTool("downparabola")}}} title="down parabola tool">
             <img src="/downparabola.png" alt="down parabola icon" style={{height: "16px", width: "16px"}}/>
           </div>
         </div>
         <div className="flex" style={{borderBottom: "1px solid black"}}>
-          <div className={`tool ${tool==="ellipse"?"selected-tool":""}`} style={{borderRight: "1px solid black"}} onClick={()=>{if(props.checkUserSignedIn()){setTool("ellipse")}}}>
+          <div className={`tool ${tool==="ellipse"?"selected-tool":""}`} style={{borderRight: "1px solid black"}} onClick={()=>{if(props.checkUserSignedIn()){setTool("ellipse")}}} title="ellipse tool">
             <img src="/ellipse.png" alt="ellipse icon" style={{height: "18px", width: "18px"}}/>
           </div>
-          <div className={`tool ${tool==="parallelogram"?"selected-tool":""}`} onClick={()=>{if(props.checkUserSignedIn()){setTool("parallelogram")}}}>
+          <div className={`tool ${tool==="parallelogram"?"selected-tool":""}`} onClick={()=>{if(props.checkUserSignedIn()){setTool("parallelogram")}}} title="parallelogram tool">
             <img src="/parallelogram.png" alt="parallelogram icon" style={{height: "20px", width: "20px"}}/>
           </div>
         </div>
         <div className="flex" style={{borderBottom: "1px solid black"}}>
-          <div className={`tool ${tool==="star"?"selected-tool":""}`} style={{borderRight: "1px solid black"}} onClick={()=>{if(props.checkUserSignedIn()){setTool("star")}}}>
+          <div className={`tool ${tool==="star"?"selected-tool":""}`} style={{borderRight: "1px solid black"}} onClick={()=>{if(props.checkUserSignedIn()){setTool("star")}}} title="star tool">
             <img src="/star.png" alt="star icon" style={{height: "20px", width: "20px"}}/>
           </div>
-          <div className={`tool ${tool==="heart"?"selected-tool":""}`} onClick={()=>{if(props.checkUserSignedIn()){setTool("heart")}}}>
+          <div className={`tool ${tool==="heart"?"selected-tool":""}`} onClick={()=>{if(props.checkUserSignedIn()){setTool("heart")}}} title="heart tool">
             <img src="/heart.png" alt="heart icon" style={{height: "20px", width: "20px"}}/>
           </div>
         </div>
