88585b2d041d4766e4d989197ef80b1f6b97b1fc
Keppiekip <maharjanitika@gmail.com>
2026-01-26 20:40:01 +0545

    refactor: setup manual cors handling

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,36 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const connectToMongo = require('./db');
+// const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT;
+const frontend_url = process.env.FRONTEND_URL;
+
+// use of cors library
+// app.use(cors({
+//   origin: frontend_url,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', '_id', 'user_id', 'authtoken'],
+//   optionsSuccessStatus: 200
+// }));
+
+// manual cors setup
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', frontend_url);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,_id,user_id,authtoken');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  next();
+});
 
 connectToMongo();
 
 app.use(express.json({ limit: '50mb' })); // increase json limit
 app.use(express.urlencoded({ limit: '50mb', extended: true })); // increase form limit
-
-app.use(cors({
-  origin: `${process.env.FRONTEND_URL}`,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', '_id', 'user_id', 'authtoken']
-}));
 
 // Routes:
 app.use('/api/admin', require('./routes/admin'));
@@ -24,6 +39,11 @@ app.use('/api/colorpalette', require('./routes/colorpalette'));
 app.use('/api/drawing', require('./routes/drawing'));
 app.use('/api/template', require('./routes/template'));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  const port = process.env.PORT;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+module.exports = app;
