f23ae866ed9641b98ee72eca86005dfb310f07d7
Keppiekip <maharjanitika@gmail.com>
2026-01-26 18:30:27 +0545

    refactor: update cors

--- backend/index.js
+++ backend/index.js
@@ -4,28 +4,18 @@ const cors = require('cors');
 const connectToMongo = require('./db');
 
 const app = express();
-// const port = process.env.PORT;
+const port = process.env.PORT;
 
 connectToMongo();
 
 app.use(express.json({ limit: '50mb' })); // increase json limit
 app.use(express.urlencoded({ limit: '50mb', extended: true })); // increase form limit
 
-// app.use(cors({
-//   origin: 'https://paint-kit-app.vercel.app',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', '_id', 'user_id', 'authtoken']
-// }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://paint-kit-app.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type,_id,user_id,authtoken");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', '_id', 'user_id', 'authtoken']
+}));
 
 // Routes:
 app.use('/api/admin', require('./routes/admin'));
@@ -34,8 +24,6 @@ app.use('/api/colorpalette', require('./routes/colorpalette'));
 app.use('/api/drawing', require('./routes/drawing'));
 app.use('/api/template', require('./routes/template'));
 
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
