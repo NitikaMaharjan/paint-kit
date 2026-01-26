beb8e44badbeecdfe54b98ae9ddc4440b0f9495b
Keppiekip <maharjanitika@gmail.com>
2026-01-26 12:57:00 +0545

    refactor: update cors

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,21 @@ connectToMongo();
 app.use(express.json({ limit: '50mb' })); // increase json limit
 app.use(express.urlencoded({ limit: '50mb', extended: true })); // increase form limit
 
-app.use(cors({
-  origin: 'https://paint-kit-app.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', '_id', 'user_id', 'authtoken']
-}));
+// app.use(cors({
+//   origin: 'https://paint-kit-app.vercel.app',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', '_id', 'user_id', 'authtoken']
+// }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://paint-kit-app.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,_id,user_id,authtoken");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 // Routes:
 app.use('/api/admin', require('./routes/admin'));
